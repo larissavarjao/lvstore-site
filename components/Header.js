4afae3671cb1704eb0938d2989bbcd5dd288cac5
Larissa <larissasilvavarjao@gmail.com>
2020-03-24 17:53:22 -0300

    feat(cart): add cart to header

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,9 @@
-import Nav from './Nav';
 import Link from 'next/link';
 import styled from 'styled-components';
 import NProgress from 'nprogress';
 import Router from 'next/router';
+import Nav from './Nav';
+import Cart from './Cart';
 
 Router.onRouteChangeStart = () => {
   NProgress.start();
@@ -69,7 +70,7 @@ const Header = () => (
     <div className='sub-bar'>
       <p>search</p>
     </div>
-    <div>Cart</div>
+    <Cart />
   </StyledHeader>
 );
 
