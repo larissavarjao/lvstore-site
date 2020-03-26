e803216634fd090c23a3559ea8156bfcf60dfa6c
Larissa <larissasilvavarjao@gmail.com>
2020-03-26 15:50:05 -0300

    test(cart): RemoveFromCart component and snapshot

--- components/RemoveFromCart.js
+++ components/RemoveFromCart.js
@@ -6,7 +6,7 @@ import gql from 'graphql-tag';
 
 import { CURRENT_USER_QUERY } from './User';
 
-const REMOVE_FROM_CART_MUTATION = gql`
+export const REMOVE_FROM_CART_MUTATION = gql`
   mutation REMOVE_FROM_CART_MUTATION($id: ID!) {
     removeFromCart(id: $id) {
       id
