7e216f8e027e141c89d844cba08c28f1c34b950f
Larissa <larissasilvavarjao@gmail.com>
2020-03-27 16:27:17 -0300

    refactor(Pagination): change to hooks

--- components/Pagination.js
+++ components/Pagination.js
@@ -6,6 +6,7 @@ import Link from 'next/link';
 
 import PaginationStyle from './styles/PaginationStyles';
 import { perPage } from '../config';
+import { useQuery } from '@apollo/react-hooks';
 
 export const PAGINATION_QUERY = gql`
   query PAGINATION_QUERY {
@@ -17,45 +18,39 @@ export const PAGINATION_QUERY = gql`
   }
 `;
 
-const Pagination = props => (
-  <Query query={PAGINATION_QUERY}>
-    {({ data, loading, error }) => {
-      if (loading) return <p>Loading...</p>;
-      if (error) return <Error error={error} />;
+const Pagination = props => {
+  const { data, loading, error } = useQuery(PAGINATION_QUERY);
 
-      const count = data.itemsConnection.aggregate.count;
-      const pages = Math.ceil(count / perPage);
-      const page = props.page;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <Error error={error} />;
 
-      return (
-        <PaginationStyle data-test='pagination'>
-          <Head>
-            <title>
-              LV Store | Page {page} of {pages}
-            </title>
-          </Head>
-          <Link
-            prefetch
-            href={{ pathname: '/items', query: { page: page - 1 } }}>
-            <a className='prev' aria-disabled={page <= 1}>
-              ⬅ Prev
-            </a>
-          </Link>
-          <p>
-            Page {page} of <span className='totalPages'>{pages}</span>!
-          </p>
-          <p>{count} Items Total</p>
-          <Link
-            prefetch
-            href={{ pathname: '/items', query: { page: page + 1 } }}>
-            <a className='next' aria-disabled={page >= pages}>
-              Next ➡
-            </a>
-          </Link>
-        </PaginationStyle>
-      );
-    }}
-  </Query>
-);
+  const count = data.itemsConnection.aggregate.count;
+  const pages = Math.ceil(count / perPage);
+  const page = props.page;
+
+  return (
+    <PaginationStyle data-test='pagination'>
+      <Head>
+        <title>
+          LV Store | Page {page} of {pages}
+        </title>
+      </Head>
+      <Link prefetch href={{ pathname: '/items', query: { page: page - 1 } }}>
+        <a className='prev' aria-disabled={page <= 1}>
+          ⬅ Prev
+        </a>
+      </Link>
+      <p>
+        Page {page} of <span className='totalPages'>{pages}</span>!
+      </p>
+      <p>{count} Items Total</p>
+      <Link prefetch href={{ pathname: '/items', query: { page: page + 1 } }}>
+        <a className='next' aria-disabled={page >= pages}>
+          Next ➡
+        </a>
+      </Link>
+    </PaginationStyle>
+  );
+};
 
 export default Pagination;
