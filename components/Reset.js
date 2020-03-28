ad12b280d0c0fc7a16662dd28f5430a5d795a722
Larissa <larissasilvavarjao@gmail.com>
2020-03-28 09:42:48 -0300

    refactor(Reset): change to hooks

--- components/Reset.js
+++ components/Reset.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { Mutation } from 'react-apollo';
+import React, { useState } from 'react';
 import gql from 'graphql-tag';
-import PropTypes from 'prop-types';
+import Router from 'next/router';
 
 import { CURRENT_USER_QUERY } from './User';
 import Form from './styles/Form';
 import Error from './ErrorMessage';
+import { useMutation } from '@apollo/react-hooks';
 
 const RESET_PASSWORD_MUTATION = gql`
   mutation RESET_PASSWORD_MUTATION(
@@ -25,69 +25,63 @@ const RESET_PASSWORD_MUTATION = gql`
   }
 `;
 
-class RequestPassword extends Component {
-  static propTypes = {
-    resetToken: PropTypes.string.isRequired
+const RequestPassword = ({ resetToken }) => {
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [requestReset, { error, loading, called }] = useMutation(
+    RESET_PASSWORD_MUTATION,
+    {
+      refetchQueries: [{ query: CURRENT_USER_QUERY }]
+    }
+  );
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    await requestReset({
+      variables: {
+        resetToken: resetToken,
+        password: password,
+        confirmPassword: confirmPassword
+      }
+    });
+    setPassword('');
+    setConfirmPassword('');
+
+    Router.push({
+      pathname: '/'
+    });
   };
 
-  state = {
-    password: '',
-    confirmPassword: ''
-  };
+  return (
+    <Form method='post' onSubmit={onSubmit}>
+      <fieldset disabled={loading} aria-busy={loading}>
+        <h2>Reset your password</h2>
+        <Error error={error} />
+        <label htmlFor='password'>
+          Password
+          <input
+            type='password'
+            name='password'
+            placeholder='Email'
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </label>
+        <label htmlFor='confirmPassword'>
+          Confirm Password
+          <input
+            type='password'
+            name='confirmPassword'
+            placeholder='Confirm Password'
+            value={confirmPassword}
+            onChange={e => setConfirmPassword(e.target.value)}
+          />
+        </label>
 
-  saveToState = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  render() {
-    return (
-      <Mutation
-        mutation={RESET_PASSWORD_MUTATION}
-        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
-        variables={{
-          resetToken: this.props.resetToken,
-          password: this.state.password,
-          confirmPassword: this.state.confirmPassword
-        }}>
-        {(requestReset, { error, loading, called }) => (
-          <Form
-            method='post'
-            onSubmit={async e => {
-              e.preventDefault();
-              await requestReset();
-              this.setState({ password: '', confirmPassword: '' });
-            }}>
-            <fieldset disabled={loading} aria-busy={loading}>
-              <h2>Reset your password</h2>
-              <Error error={error} />
-              <label htmlFor='password'>
-                Password
-                <input
-                  type='password'
-                  name='password'
-                  placeholder='Email'
-                  value={this.state.password}
-                  onChange={this.saveToState}
-                />
-              </label>
-              <label htmlFor='confirmPassword'>
-                Confirm Password
-                <input
-                  type='password'
-                  name='confirmPassword'
-                  placeholder='Confirm Password'
-                  value={this.state.confirmPassword}
-                  onChange={this.saveToState}
-                />
-              </label>
-
-              <button type='submit'>Request Reset!</button>
-            </fieldset>
-          </Form>
-        )}
-      </Mutation>
-    );
-  }
-}
+        <button type='submit'>Request Reset!</button>
+      </fieldset>
+    </Form>
+  );
+};
 
 export default RequestPassword;
