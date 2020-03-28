feef2993e2f525db9e746b5a2996cf3df576b5dd
Larissa <larissasilvavarjao@gmail.com>
2020-03-28 10:13:25 -0300

    refactor(Signout): change to hooks

--- components/Signout.js
+++ components/Signout.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import { CURRENT_USER_QUERY } from './User';
+import { useMutation } from '@apollo/react-hooks';
 
 const SIGN_OUT_MUTATION = gql`
   mutation SIGN_OUT_MUTATION {
@@ -11,12 +11,12 @@ const SIGN_OUT_MUTATION = gql`
   }
 `;
 
-const SignOut = props => (
-  <Mutation
-    mutation={SIGN_OUT_MUTATION}
-    refetchQueries={[{ query: CURRENT_USER_QUERY }]}>
-    {signout => <button onClick={signout}>Sign Out</button>}
-  </Mutation>
-);
+const SignOut = () => {
+  const [signout] = useMutation(SIGN_OUT_MUTATION, {
+    refetchQueries: [{ query: CURRENT_USER_QUERY }]
+  });
+
+  return <button onClick={signout}>Sign Out</button>;
+};
 
 export default SignOut;
